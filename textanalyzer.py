c22ba83413a18f1c493b1fe265a9e6aeaa31d6c9
Andreas Olsson <andreasolsson2020@outlook.com>
2023-07-30 19:17:45 +0200

    create structure

---
+++ textanalyzer.py
@@ -0,0 +1,11 @@
+class TextAnalyzer:
+    def text_analyzer(self,sentence,keywords):
+        raise NotImplementedError("Denna del av koden är inte implementerad ännu.")
+
+def main():
+    analyse = TextAnalyzer()
+    raise NotImplementedError("Denna del av koden är inte implementerad ännu.")
+
+
+if __name__ == "__main__":
+    main()
