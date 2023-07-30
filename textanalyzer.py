3605c131bde029bbe2efcdb4fc6f08b35ae8903e
Andreas Olsson <andreasolsson2020@outlook.com>
2023-07-30 19:20:07 +0200

    add welcome text

--- textanalyzer.py
+++ textanalyzer.py
@@ -4,6 +4,7 @@ class TextAnalyzer:
 
 def main():
     analyse = TextAnalyzer()
+    print("Welcome to text analyzer")
     raise NotImplementedError("Denna del av koden är inte implementerad ännu.")
 
 
