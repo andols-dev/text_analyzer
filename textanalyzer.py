9a4360e3fde4aa78e81fe2a139e1402b606fce63
Andreas Olsson <andreasolsson2020@outlook.com>
2023-07-30 19:43:45 +0200

    change copys to copies

--- textanalyzer.py
+++ textanalyzer.py
@@ -5,10 +5,12 @@ class TextAnalyzer:
 
 def main():
     analyze = TextAnalyzer()
+
     print("Welcome to text analyzer")
     print("Copy text from, for example, a webpage or document.")
     print("Add keyword that you want look for. Use space between every keyword")
-    # The text that user copys will be added to clipboard and will be inserted in sentence
+
+    # The text that user copies will be added to clipboard and will be inserted in sentence
     sentence = pyperclip.paste()
     raise NotImplementedError("Denna del av koden är inte implementerad ännu.")
 
