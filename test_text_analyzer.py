2f9abb19391978c2ab81f8b4e26bb82c29cc91ce
Andreas Olsson <andreasolsson2020@outlook.com>
2023-08-03 09:46:30 +0200

    create test_check_occurences_lower

--- test_text_analyzer.py
+++ test_text_analyzer.py
@@ -3,7 +3,7 @@ from unittest.mock import patch
 from textanalyzer import TextAnalyzer
 
 
-class TestYourClass(unittest.TestCase):
+class TestText(unittest.TestCase):
 
     def test_is_there_input(self):
         analyzer = TextAnalyzer()
@@ -25,6 +25,25 @@ class TestYourClass(unittest.TestCase):
         with patch('builtins.print') as mock_print:
             analyzer.how_manywords(test_text)
 
+
+
         mock_print.assert_any_call("Occurrences of each word in the text")
+
+    def test_check_occurences_lower(self):
+        analyzer = TextAnalyzer()
+        test_text = "Detta är ett test. detta"
+
+        expected_output = {
+            "Detta": 2,
+            "är": 1,
+            "ett": 1,
+            "test": 1,
+        }
+
+        with patch('builtins.print') as mock_print:
+            analyzer.how_manywords(test_text)
+
+        mock_print.assert_any_call("Occurrences of each word in the text")
+
 if __name__ == '__main__':
     unittest.main()
