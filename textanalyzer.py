0e23d9aec049435bc414a25972ef497c0ac689fe
Andreas Olsson <andreasolsson2020@outlook.com>
2023-08-13 15:55:58 +0200

    define static methods

--- textanalyzer.py
+++ textanalyzer.py
@@ -1,6 +1,6 @@
 class TextAnalyzer:
     @staticmethod
-    def make_alphanumeric(self, word):
+    def make_alphanumeric(word):
         """
         Make the word alphanumeric.
         :param word: The input word.
@@ -9,7 +9,8 @@ class TextAnalyzer:
         cleaned_text = ''.join(ch for ch in word if ch.isalpha())
         return cleaned_text
 
-    def how_many_words(self, text):
+    @staticmethod
+    def how_many_words(text):
         """
         Count the occurrences of each word in the given text.
         :param text: The input text.
@@ -18,7 +19,7 @@ class TextAnalyzer:
         print("Occurrences of each word in the text")
 
         for word in text.split():
-            word_occurences[self.make_alphanumeric(word).lower()] = 1 + word_occurences.get(word, 0)
+            word_occurences[TextAnalyzer.make_alphanumeric(word).lower()] = 1 + word_occurences.get(word, 0)
 
         for word, count in word_occurences.items():
             print(f"{word}: {count}")
@@ -31,10 +32,11 @@ class TextAnalyzer:
         """
         unique_words = set()
         for word in text.split():
-            unique_words.add(self.make_alphanumeric(word).lower())
+            unique_words.add(TextAnalyzer.make_alphanumeric(word).lower())
         return len(unique_words)
 
-    def most_common_words(self, text, num_words):
+    @staticmethod
+    def most_common_words(text, num_words):
         """
         Return the most common words and their occurrence count in the given text.
         :param text: The input text.
@@ -43,7 +45,7 @@ class TextAnalyzer:
         """
         word_freq = {}
         for word in text.split():
-            word_freq[self.make_alphanumeric(word).lower()] = word_freq.get(self.make_alphanumeric(word).lower(), 0) + 1
+            word_freq[TextAnalyzer.make_alphanumeric(word).lower()] = word_freq.get(TextAnalyzer.make_alphanumeric(word).lower(), 0) + 1
 
         sorted_word_freq = sorted(word_freq.items(), key=lambda item: item[1], reverse=True)
         return sorted_word_freq[:num_words]
@@ -83,8 +85,8 @@ class TextAnalyzer:
             choice = input("Enter the letter of the option you want to run and press enter: \n")
 
             if choice == 'a':
-                num_words = self.choose_how_many_words()
-                common_words = self.most_common_words(text, num_words)
+                num_words = TextAnalyzer.choose_how_many_words()
+                common_words = TextAnalyzer.most_common_words(text, num_words)
                 print("Most common words in the text:")
                 for word, count in common_words:
                     print(f"{word}: {count}")
@@ -92,7 +94,7 @@ class TextAnalyzer:
                 unique_words_count = self.count_unique_words(text)
                 print(f"Number of unique words: {unique_words_count}")
             elif choice == 'c':
-                self.how_many_words(text)
+                TextAnalyzer.how_many_words(text)
             elif choice == 'x':
                 print("Exiting...")
                 break
